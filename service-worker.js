5568760527b25f706dd5c3a4711f6d50fa018fe1
stefanoruscito <80580708+stefanoruscito@users.noreply.github.com>
2025-07-18 22:46:31 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,32 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('quiz-alterazioni').then(cache => {
-      return cache.addAll([
-        './index.html',
-        './manifest.json',
-        './icon.png'
-      ]);
-    })
+const CACHE_NAME = 'quiz-alterazioni-v3';
+const FILES_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
